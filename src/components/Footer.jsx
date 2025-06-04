3d6b41447b4b0bb169014fd6d713ddbf4c4bd693
Muhammad Ikhwananda rizaldi <79822804+Ikhwanand@users.noreply.github.com>
2025-06-04 17:16:24 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
             <div className="flex flex-col md:flex-row justify-between items-center">
                 <div className="mb-4 md:mb-0">
                     <p className="text-sm text-lightText">
-                        &copy; {new Date().getFullYear()} Skin Doctor AI. All rights reserved.
+                        &copy; {new Date().getFullYear()} RonaAI. All rights reserved.
                     </p>
                 </div>
                 <div className="flex space-x-6">
